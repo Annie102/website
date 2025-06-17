b9c976b8e96d23e4cd8f1a742ba920402d0a1415
林靚婷 <annie107@linjingtingdeMacBook-Pro.local>
2025-06-17 23:00:43 +0800

    whole update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,34 @@
 /** @type {import('next').NextConfig} */
-const repoName = 'website'; // 🔁 替換成你的實際 repo 名稱
+// const repoName = 'website'; // 🔁 替換成你的實際 repo 名稱
 
-export default {
+// export default {
+//   output: 'export',
+//   basePath: '/' + repoName,
+//   assetPrefix: '/' + repoName + '/',
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true, 
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  basePath: '/' + repoName,
-  assetPrefix: '/' + repoName + '/',
+  basePath: '/website',
   images: {
-    unoptimized: true,
+      unoptimized: true,
+      loader: 'custom',
+      loaderFile: './image-loader.js',
   },
-  trailingSlash: true, 
+  assetPrefix: '/react-test',
+  trailingSlash: true,
+  webpack: (config) => {
+      config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          type: 'asset/resource',
+      });
+      return config;
+  },
 };
 
+export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
-export default {
-    content: [
-        './src/app/**/*.{js,jsx}',
-        './src/pages/**/*.{js,jsx}',
-        './src/components/**/*.{js,jsx}',
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+// export default {
+//     content: [
+//         './src/app/**/*.{js,jsx}',
+//         './src/pages/**/*.{js,jsx}',
+//         './src/components/**/*.{js,jsx}',
+//         './src/**/*.{js,ts,jsx,tsx}',
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+      extend: {
+          colors: {
+              background: 'var(--background)',
+              foreground: 'var(--foreground)',
+          },
+          fontFamily: {
+              sans: ['var(--font-inter)'],
+              mono: ['var(--font-inter)'],
+          },
+      },
+  },
+  plugins: [],
+}
